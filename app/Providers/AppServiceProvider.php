ab300c76d006d8a0646ba259832facf1ea7e203e
hanafisg196 <hanafisg196@gmail.com>
2026-07-27 15:57:07 +0700

    dhdhds

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,25 +1,27 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Pagination\Paginator;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
-use Livewire\Livewire;
-use App\Http\Controllers\CustomLivewireController;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
         if (config('app.env') === 'production') {
@@ -29,18 +31,17 @@ class AppServiceProvider extends ServiceProvider
         app()->setLocale(config('app.locale'));
         $this->configureDefaults();
         Paginator::defaultView('vendor.pagination.bootstrap-5');
-
-        Livewire::setUploadRoute(function ($handle) {
-            return Route::post('/livewire/upload-file', $handle)
-                ->middleware(['web'])
-                ->name('livewire.upload-file');
-        });
     }
 
+    /**
+     * Configure default behaviors for production-ready applications.
+     */
     protected function configureDefaults(): void
     {
         Date::use(CarbonImmutable::class);
+
         DB::prohibitDestructiveCommands(app()->isProduction());
+
         Password::defaults(fn(): ?Password => app()->isProduction() ? Password::min(12)->mixedCase()->letters()->numbers()->symbols()->uncompromised() : null);
     }
 }
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\LaporanServiceProvider::class,
+    App\Providers\LivewireOverrideServiceProvider::class,
     App\Providers\MainServiceProvider::class,
     App\Providers\SetoranServiceProvider::class,
     App\Providers\TransaksiServiceProvider::class,
