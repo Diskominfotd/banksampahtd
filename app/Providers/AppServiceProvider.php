f033dea4846b2dcc9940e48da24bcb8f0e382fa5
hanafisg196 <hanafisg196@gmail.com>
2026-07-27 10:16:52 +0700

    dd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
         Carbon::setLocale(config('app.locale'));
         app()->setLocale(config('app.locale'));
         $this->configureDefaults();
