fbcc71467df34bf097fccd228866a5fe18ce0605
hanafisg196 <hanafisg196@gmail.com>
2026-09-24 08:42:57 +0700

    fix nix column

--- database/migrations/2026_09_24_014202_set_nik_nullable_text_to_users.php
+++ database/migrations/2026_09_24_014202_set_nik_nullable_text_to_users.php
@@ -4,19 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nik')->nullable()->change();
+            $table->text('nik')->nullable()->change();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nik')->nullable(false)->change();
+            $table->text('nik')->nullable(false)->change();
         });
     }
 };
